6fb85016e4e86686926436a734884fac8c9018f3
KaiOelfke <k.oelfke@me.com>
2025-05-06 15:44:48 +0800

    fix more imports

--- ReactiveObjC/NSObject+RACAppKitBindings.m
+++ ReactiveObjC/NSObject+RACAppKitBindings.m
@@ -7,8 +7,8 @@
 //
 
 #import "NSObject+RACAppKitBindings.h"
-#import <ReactiveObjC/EXTKeyPathCoding.h>
-#import <ReactiveObjC/EXTScope.h>
+#import "RACEXTKeyPathCoding.h"
+#import "RACEXTScope.h"
 #import "NSObject+RACDeallocating.h"
 #import "RACChannel.h"
 #import "RACCompoundDisposable.h"
--- ReactiveObjCTests/NSObjectRACAppKitBindingsSpec.m
+++ ReactiveObjCTests/NSObjectRACAppKitBindingsSpec.m
@@ -11,7 +11,7 @@
 
 #import "RACChannelExamples.h"
 
-#import <ReactiveObjC/EXTKeyPathCoding.h>
+#import "RACEXTKeyPathCoding.h"
 #import "NSObject+RACAppKitBindings.h"
 
 QuickSpecBegin(NSObjectRACAppKitBindingsSpec)
